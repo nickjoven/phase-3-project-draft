ddb42a6615758c5bf4054c67339364a8187e86d4
Nicholas <nicholasjoven@gmail.com>
2022-08-26 04:56:08 -0400

    main page has routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import Feed from './components/Feed'
 import NavBar from './components/NavBar'
 import MiniSequencer from "./components/MiniSequencer";
 import SignUp from './components/SignUp';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 import Content from './components/Content';
 import SequenceEditor from './components/SequenceEditor';
+import { BrowserRouter as Router, Routes, Route, useRoutes } from 'react-router-dom'
 
 
 const App = () => {
@@ -34,35 +34,52 @@ const App = () => {
           return <p>loading....</p>;
         }
         return (
-          <div className='App'>
-             <div className='appcont' style={{display: currentUser?'grid':'block', padding: currentUser?'20px 20px 0 20px':'0'}}></div>
-            <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser} isLoggedIn={isLoggedIn} />
-            {isLoggedIn ?
-            <>
-              <Content /> 
-            </> :
-            <>
-            {hasAccount ? ( 
-              <>
-              <Login setHasAccount = {() => setHasAccount(false)} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-              </>
-            ) : ( 
-              <SignUp currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-            )}
-              <MiniSequencer player={player} lineMap={lineMap} />
-            </>
-            }          
-            <SequenceEditor 
-              player={player} 
-              lineMap={lineMap2} 
-              setCrusherValue={setCrusherValue} 
-              crusherValue={crusherValue} 
-              limiterValue={limiterValue}
-              setLimiterValue={setLimiterValue}
-              distortionValue={distortionValue}
-              setDistortionValue={setDistortionValue}
-            />
-          </div>
+          <Router>
+            <div className='App'>
+              <div className='appcont' style={{display: currentUser?'grid':'block', padding: currentUser?'20px 20px 0 20px':'0'}}></div>
+              <NavBar currentUser={currentUser} setCurrentUser={setCurrentUser} isLoggedIn={isLoggedIn} />
+              <Routes>
+                <Route path="/" element={isLoggedIn ?
+                  <>
+                    <Content /> 
+                  </> :
+                  <>
+                    {hasAccount ? 
+                      <>
+                        <Login setHasAccount = {() => setHasAccount(false)} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+                      </> : 
+                      <SignUp currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+                    }
+                  <MiniSequencer player={player} lineMap={lineMap} />
+                  </>
+                }/>
+                <Route path="/sequencer" element={
+                  <SequenceEditor 
+                    player={player} 
+                    lineMap={lineMap2} 
+                    setCrusherValue={setCrusherValue} 
+                    crusherValue={crusherValue} 
+                    limiterValue={limiterValue}
+                    setLimiterValue={setLimiterValue}
+                    distortionValue={distortionValue}
+                    setDistortionValue={setDistortionValue}
+                    />
+                } />
+                <Route path='login' element={
+                  <>
+                    <Login setHasAccount={() => setHasAccount(false)} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                    <MiniSequencer player={player} lineMap={lineMap} />     
+                  </>}
+                />
+                <Route path='signup' element={
+                  <>
+                    <SignUp currentUser={currentUser} setCurrentUser={setCurrentUser} />
+                    <MiniSequencer player={player} lineMap={lineMap} /> 
+                  </>}
+                />          
+                </Routes>
+            </div>
+          </Router>
         )
       }}
     </PlayerProvider>
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import 'bulma/css/bulma.min.css'
-import { Provider } from 'react-redux';
-import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
